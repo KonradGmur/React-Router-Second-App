b57fcdef638c3dd5bded025feed49d13df9030a1
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-13 16:49:49 +0200

    Update finish app

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -15,7 +15,24 @@ const Header = () => {
 
     return (
         <>
-
+            <Switch>
+                <Route path="/" exact render={() => (
+                    <img src={img1} alt="miasto" />
+                )} />
+                <Route path="/products" render={() => (
+                    <img src={img2} alt="miasto" />
+                )} />
+                <Route path="/contact" render={() => (
+                    <img src={img3} alt="miasto" />
+                )} />
+                <Route path="/admin" render={() => (
+                    <img src={img3} alt="miasto" />
+                )} />
+                <Route render={() => (
+                    <img src={img1} alt="miasto" />
+                )} />
+            </Switch>
+            {/* <img src={img} alt="header" /> */}
         </>
     );
 }
